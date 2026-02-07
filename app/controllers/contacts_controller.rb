b8f1e6145b512ed031f14c705b6a1382a3a7bf39
donnie-ccama <donnie@citykid.me>
2026-02-07 01:43:46 -0600

    Add comprehensive Contact CRUD forms with all NPSP fields

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,10 +1,15 @@
 class ContactsController < ApplicationController
+  before_action :set_contact, only: [:show, :edit, :update, :destroy]
+  before_action :load_accounts, only: [:new, :edit, :create, :update]
+
   def index
     @contacts = Contact.includes(:account).order(created_at: :desc).page(params[:page]).per(25)
   end
 
   def show
-    @contact = Contact.find(params[:id])
+    @recent_donations = @contact.donations.order(donation_date: :desc).limit(5)
+    @recurring_donations = @contact.recurring_donations.limit(5)
+    @affiliations = @contact.affiliations.includes(:organization).limit(5)
   end
 
   def new
@@ -12,7 +17,6 @@ class ContactsController < ApplicationController
   end
 
   def edit
-    @contact = Contact.find(params[:id])
   end
 
   def create
@@ -25,7 +29,6 @@ class ContactsController < ApplicationController
   end
 
   def update
-    @contact = Contact.find(params[:id])
     if @contact.update(contact_params)
       redirect_to @contact, notice: 'Contact was successfully updated.'
     else
@@ -34,14 +37,29 @@ class ContactsController < ApplicationController
   end
 
   def destroy
-    @contact = Contact.find(params[:id])
     @contact.destroy
     redirect_to contacts_url, notice: 'Contact was successfully deleted.'
   end
 
   private
 
+  def set_contact
+    @contact = Contact.find(params[:id])
+  end
+
+  def load_accounts
+    @accounts = Account.order(:name)
+  end
+
   def contact_params
-    params.require(:contact).permit(:first_name, :last_name, :email, :phone, :mobile, :account_id, :title, :birthdate)
+    params.require(:contact).permit(
+      :first_name, :last_name, :salutation, :title, :department,
+      :account_id, :primary_affiliation_id,
+      :personal_email, :work_email, :alternate_email, :preferred_email,
+      :home_phone, :mobile_phone, :work_phone, :other_phone, :preferred_phone,
+      :mailing_street, :mailing_city, :mailing_state, :mailing_postal_code, :mailing_country,
+      :birthdate, :lead_source, :description,
+      :deceased, :do_not_contact, :do_not_call, :email_opt_out
+    )
   end
 end
