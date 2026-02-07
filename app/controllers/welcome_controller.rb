0b643d708ffc319355ac8411d64913ca058d8b12
donnie-ccama <donnie@citykid.me>
2026-02-07 00:41:10 -0600

    Add database connection error reporting

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,12 @@
 class WelcomeController < ApplicationController
   def index
+    begin
+      ActiveRecord::Base.connection.execute("SELECT 1")
+      @db_status = "Connected! (Safe to use)"
+      @db_error = nil
+    rescue => e
+      @db_status = "Connection Failed"
+      @db_error = e.message
+    end
   end
 end
