6394d54c3336126ffc4404ef31337479eb363738
donnie-ccama <donnie@citykid.me>
2026-02-07 01:50:21 -0600

    Add comprehensive Account CRUD with type-specific sections

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,10 +1,15 @@
 class AccountsController < ApplicationController
+  before_action :set_account, only: [:show, :edit, :update, :destroy]
+  before_action :load_parent_accounts, only: [:new, :edit, :create, :update]
+
   def index
     @accounts = Account.order(created_at: :desc).page(params[:page]).per(25)
   end
 
   def show
-    @account = Account.find(params[:id])
+    @contacts = @account.contacts.limit(10)
+    @recent_donations = @account.donations.order(donation_date: :desc).limit(5)
+    @recurring_donations = @account.recurring_donations.limit(5)
   end
 
   def new
@@ -12,7 +17,6 @@ class AccountsController < ApplicationController
   end
 
   def edit
-    @account = Account.find(params[:id])
   end
 
   def create
@@ -25,7 +29,6 @@ class AccountsController < ApplicationController
   end
 
   def update
-    @account = Account.find(params[:id])
     if @account.update(account_params)
       redirect_to @account, notice: 'Account was successfully updated.'
     else
@@ -34,14 +37,29 @@ class AccountsController < ApplicationController
   end
 
   def destroy
-    @account = Account.find(params[:id])
     @account.destroy
     redirect_to accounts_url, notice: 'Account was successfully deleted.'
   end
 
   private
 
+  def set_account
+    @account = Account.find(params[:id])
+  end
+
+  def load_parent_accounts
+    @parent_accounts = Account.where(account_type: 'organization').where.not(id: @account&.id).order(:name)
+  end
+
   def account_params
-    params.require(:account).permit(:name, :account_type, :phone, :website, :description)
+    params.require(:account).permit(
+      :name, :account_type, :record_type,
+      :phone, :fax, :website,
+      :billing_street, :billing_city, :billing_state, :billing_postal_code, :billing_country,
+      :shipping_street, :shipping_city, :shipping_state, :shipping_postal_code, :shipping_country,
+      :industry, :annual_revenue, :number_of_employees,
+      :formal_greeting, :informal_greeting,
+      :parent_account_id, :description
+    )
   end
 end
