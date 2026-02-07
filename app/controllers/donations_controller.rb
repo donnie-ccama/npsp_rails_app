8e7601c8e1a145a1b5975412337906c3bd2391a2
donnie-ccama <donnie@citykid.me>
2026-02-07 01:57:50 -0600

    Add Donations CRUD with tribute gifts and acknowledgment tracking

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -1,18 +1,20 @@
 class DonationsController < ApplicationController
+  before_action :set_donation, only: [:show, :edit, :update, :destroy]
+  before_action :load_associations, only: [:new, :edit, :create, :update]
+
   def index
-    @donations = Donation.includes(:contact, :account, :campaign).order(donation_date: :desc).page(params[:page]).per(25)
+    @donations = Donation.includes(:contact, :account, :campaign).order(close_date: :desc).page(params[:page]).per(25)
   end
 
   def show
-    @donation = Donation.find(params[:id])
+    @payments = @donation.payments.order(payment_date: :desc)
   end
 
   def new
-    @donation = Donation.new
+    @donation = Donation.new(close_date: Date.current, stage: :pledged)
   end
 
   def edit
-    @donation = Donation.find(params[:id])
   end
 
   def create
@@ -25,7 +27,6 @@ class DonationsController < ApplicationController
   end
 
   def update
-    @donation = Donation.find(params[:id])
     if @donation.update(donation_params)
       redirect_to @donation, notice: 'Donation was successfully updated.'
     else
@@ -34,14 +35,30 @@ class DonationsController < ApplicationController
   end
 
   def destroy
-    @donation = Donation.find(params[:id])
     @donation.destroy
     redirect_to donations_url, notice: 'Donation was successfully deleted.'
   end
 
   private
 
+  def set_donation
+    @donation = Donation.find(params[:id])
+  end
+
+  def load_associations
+    @contacts = Contact.order(:last_name, :first_name)
+    @accounts = Account.order(:name)
+    @campaigns = Campaign.order(:name)
+  end
+
   def donation_params
-    params.require(:donation).permit(:contact_id, :account_id, :campaign_id, :amount, :donation_date, :donation_type, :status, :payment_method)
+    params.require(:donation).permit(
+      :name, :contact_id, :account_id, :campaign_id, :recurring_donation_id,
+      :amount, :close_date, :stage, :probability, :record_type, :donation_type,
+      :lead_source, :next_step, :description,
+      :gift_type, :acknowledgment_status, :acknowledgment_date,
+      :tribute_type, :honoree_name, :honoree_contact_id, :notification_recipient_id,
+      :matching_gift_status, :matching_gift_account_id, :matching_gift_donation_id
+    )
   end
 end
